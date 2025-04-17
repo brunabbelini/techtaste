66d2d53e6c40d275ab10c4c46feb856dbd478a52
bububelini <bububelini@gmail.com>
2025-04-17 13:18:16 +0000

    Build a restaurant details screen and allow you to add dishes to the bag

---
+++ lib/ui/restaurant/restaurant_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/model/dish.dart';
+import 'package:myapp/model/restaurant.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  final Restaurant restaurant;
+  const RestaurantScreen({super.key, required this.restaurant});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(restaurant.name)),
+      body: Center(
+        child: Column(
+          spacing: 12.0,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/${restaurant.imagePath}', width: 128),
+            Text(
+              "Mais pedidos",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              children: List.generate(restaurant.dishes.length, (index) {
+                Dish dish = restaurant.dishes[index];
+                return ListTile(
+                  onTap: () {},
+                  leading: Image.asset(
+                    'assets/dishes/default.png',
+                    width: 48,
+                    height: 48,
+                  ),
+                  title: Text(dish.name),
+                  subtitle: Text("R\$${dish.price.toStringAsFixed(2)}"),
+                  trailing: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                );
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
